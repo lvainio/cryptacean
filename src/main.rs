7f3c12ad93495ade0f48791046980a8dcb9f0c8f
lvainio <lvainio@kth.se>
2024-10-08 21:28:09 +0200

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::time::Instant;
+pub mod hasher;
 
-use crate::hash::md5::hash;
-
-pub mod hash;
-
-fn main() {
-    let start = Instant::now();
-
-    let input: String = "rust".repeat(10_000);
-
-    let input = input.trim().as_bytes();
-
-    let hashi = hash(input);
-
-    let duration = start.elapsed();
-
-    println!("Time elapsed: {:?}", duration);
-    println!("Time elapsed: {}", hashi);
-}
+fn main() {}
