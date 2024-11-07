ef2b9014438e7ee88eceb215a88ee479b1ac3d7f
Leo Vainio <61742173+lvainio@users.noreply.github.com>
2024-11-07 13:23:53 +0100

    feat: implement sha3-224 (#30)

--- src/hash.rs
+++ src/hash.rs
@@ -4,7 +4,7 @@ pub mod md;
 pub mod sha;
 
 use md::{md2, md4, md5};
-use sha::{sha0, sha1, sha224, sha256, sha384, sha512, sha512_224, sha512_256};
+use sha::{sha0, sha1, sha224, sha256, sha384, sha3_224, sha512, sha512_224, sha512_256};
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
 pub enum HashType {
@@ -13,6 +13,7 @@ pub enum HashType {
     MD5,
     SHA0,
     SHA1,
+    SHA3_224,
     SHA224,
     SHA256,
     SHA384,
@@ -66,6 +67,14 @@ impl Output {
         }
         Output::from_u8(output_u8)
     }
+
+    pub fn from_u64_le_drop_4_bytes(output_u64: Vec<u64>) -> Output {
+        let mut output_u8: Vec<u8> = Vec::new();
+        for &value in &output_u64 {
+            output_u8.extend_from_slice(&value.to_le_bytes());
+        }
+        Output::from_u8(output_u8[..output_u8.len() - 4].to_vec())
+    }
 }
 
 pub trait HashFunction {
@@ -84,6 +93,7 @@ impl Hasher {
             HashType::MD5 => Box::new(md5::MD5),
             HashType::SHA0 => Box::new(sha0::SHA0),
             HashType::SHA1 => Box::new(sha1::SHA1),
+            HashType::SHA3_224 => Box::new(sha3_224::SHA3_224),
             HashType::SHA256 => Box::new(sha256::SHA256),
             HashType::SHA224 => Box::new(sha224::SHA224),
             HashType::SHA384 => Box::new(sha384::SHA384),
---
+++ src/hash/sha/sha3_224.rs
@@ -0,0 +1,224 @@
+use itertools::iproduct;
+
+use crate::hash::{HashFunction, Input, Output};
+
+const RHO_TABLE: [[u32; 5]; 5] = [
+    [0, 36, 3, 41, 18],
+    [1, 44, 10, 45, 2],
+    [62, 6, 43, 15, 61],
+    [28, 55, 25, 21, 56],
+    [27, 20, 39, 8, 78],
+];
+
+const PI_TABLE: [[(usize, usize); 5]; 5] = [
+    [(0, 0), (1, 3), (2, 1), (3, 4), (4, 2)],
+    [(0, 2), (1, 0), (2, 3), (3, 1), (4, 4)],
+    [(0, 4), (1, 2), (2, 0), (3, 3), (4, 1)],
+    [(0, 1), (1, 4), (2, 2), (3, 0), (4, 3)],
+    [(0, 3), (1, 1), (2, 4), (3, 2), (4, 0)],
+];
+
+const RC_TABLE: [u64; 24] = [
+    0x0000000000000001,
+    0x0000000000008082,
+    0x800000000000808a,
+    0x8000000080008000,
+    0x000000000000808b,
+    0x0000000080000001,
+    0x8000000080008081,
+    0x8000000000008009,
+    0x000000000000008a,
+    0x0000000000000088,
+    0x0000000080008009,
+    0x000000008000000a,
+    0x000000008000808b,
+    0x800000000000008b,
+    0x8000000000008089,
+    0x8000000000008003,
+    0x8000000000008002,
+    0x8000000000000080,
+    0x000000000000800a,
+    0x800000008000000a,
+    0x8000000080008081,
+    0x8000000000008080,
+    0x0000000080000001,
+    0x8000000080008008,
+];
+
+fn pad(input: &mut Vec<u8>) -> Vec<u64> {
+    let input_length: usize = input.len();
+
+    if input_length % 144 == 143 {
+        input.push(0x86); // 1000 0110
+    } else {
+        let num_zero_bytes = 144 - (input_length % 144) - 2;
+        input.push(0x06); // 0000 0110
+        input.resize(input.len() + num_zero_bytes, 0x00); // 0000 0000
+        input.push(0x80); // 1000 0000
+    }
+
+    let mut words: Vec<u64> = Vec::new();
+    for c in input.chunks_exact(8) {
+        let word = u64::from_le_bytes([c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]]);
+        words.push(word);
+    }
+
+    words
+}
+
+struct State {
+    array: [[u64; 5]; 5],
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            array: [[0u64; 5]; 5],
+        }
+    }
+
+    fn get_lane(&self, x: usize, y: usize) -> u64 {
+        self.array[x][y]
+    }
+
+    fn set_lane(&mut self, x: usize, y: usize, val: u64) {
+        self.array[x][y] = val;
+    }
+}
+
+pub struct SHA3_224;
+
+impl SHA3_224 {
+    fn theta(state: State) -> State {
+        let mut c = [0u64; 5];
+        for x in 0..5 {
+            c[x] = state.get_lane(x, 0)
+                ^ state.get_lane(x, 1)
+                ^ state.get_lane(x, 2)
+                ^ state.get_lane(x, 3)
+                ^ state.get_lane(x, 4);
+        }
+
+        let mut d = [0u64; 5];
+        for x in 0..5 {
+            d[x] = c[(x + 4) % 5] ^ c[(x + 1) % 5].rotate_left(1);
+        }
+
+        let mut new_state = State::new();
+        for (x, y) in iproduct!(0..5, 0..5) {
+            new_state.set_lane(x, y, state.get_lane(x, y) ^ d[x]);
+        }
+
+        new_state
+    }
+
+    fn rho(state: &mut State) {
+        for (x, y) in iproduct!(0..5, 0..5) {
+            state.array[x][y] = state.array[x][y].rotate_left(RHO_TABLE[x][y]);
+        }
+    }
+
+    fn pi(state: State) -> State {
+        let mut new_state = State::new();
+        for (x, y) in iproduct!(0..5, 0..5) {
+            let (to_x, to_y) = PI_TABLE[x][y];
+            new_state.array[to_x][to_y] = state.array[x][y];
+        }
+        new_state
+    }
+
+    fn chi(state: State) -> State {
+        let mut new_state = State::new();
+        for (x, y) in iproduct!(0..5, 0..5) {
+            new_state.array[x][y] =
+                state.array[x][y] ^ (!state.array[(x + 1) % 5][y] & state.array[(x + 2) % 5][y]);
+        }
+        new_state
+    }
+
+    fn iota(state: &mut State, rnd: usize) {
+        state.array[0][0] ^= RC_TABLE[rnd];
+    }
+}
+
+impl HashFunction for SHA3_224 {
+    fn hash(&self, input: &Input) -> Output {
+        let padded_input: Vec<u64> = pad(&mut input.bytes.clone());
+        let mut state = State::new();
+
+        for block in padded_input.chunks(18) {
+            state.array[0][0] ^= block[0];
+            state.array[1][0] ^= block[1];
+            state.array[2][0] ^= block[2];
+            state.array[3][0] ^= block[3];
+            state.array[4][0] ^= block[4];
+
+            state.array[0][1] ^= block[5];
+            state.array[1][1] ^= block[6];
+            state.array[2][1] ^= block[7];
+            state.array[3][1] ^= block[8];
+            state.array[4][1] ^= block[9];
+
+            state.array[0][2] ^= block[10];
+            state.array[1][2] ^= block[11];
+            state.array[2][2] ^= block[12];
+            state.array[3][2] ^= block[13];
+            state.array[4][2] ^= block[14];
+
+            state.array[0][3] ^= block[15];
+            state.array[1][3] ^= block[16];
+            state.array[2][3] ^= block[17];
+
+            for rnd in 0..24 {
+                state = Self::theta(state);
+                Self::rho(&mut state);
+                state = Self::pi(state);
+                state = Self::chi(state);
+                Self::iota(&mut state, rnd);
+            }
+        }
+
+        Output::from_u64_le_drop_4_bytes(vec![
+            state.array[0][0],
+            state.array[1][0],
+            state.array[2][0],
+            state.array[3][0],
+        ])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hash_works() {
+        let hasher = SHA3_224;
+        let i1 = Input::from_string("abc");
+        let i2 = Input::from_string("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+
+        assert_eq!(
+            hasher.hash(&i1).output,
+            "e642824c3f8cf24ad09234ee7d3c766fc9a3a5168d0c94ad73b46fdf"
+        );
+        assert_eq!(
+            hasher.hash(&i2).output,
+            "543e6868e1666c1a643630df77367ae5a62a85070a51c14cbf665cbc"
+        );
+    }
+
+    #[test]
+    fn hash_works_on_special_characters() {
+        let hasher = SHA3_224;
+        let i1 = Input::from_string("こんにちは, 世界! 😊✨");
+        let i2 = Input::from_string("안녕하세요, 세상! 🌏🎉");
+        assert_eq!(
+            hasher.hash(&i1).output,
+            "ef6ad6a3c062bc9f3afae2608b2441fdd4a342ce4be1fddaf9452758"
+        );
+        assert_eq!(
+            hasher.hash(&i2).output,
+            "f40e14a754c583754022c24acab43103666e9678351eb42ff0c39b98"
+        );
+    }
+}
