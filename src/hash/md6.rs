bee0e9b934709580b3601847a28a5e0948d26462
lvainio <lvainio@kth.se>
2024-11-09 12:25:57 +0100

    WIP: added compression function

--- src/hash/md6.rs
+++ src/hash/md6.rs
@@ -1,4 +1,8 @@
+use std::fmt;
+
 use crate::hash::{Input, Output};
+
+const WORD_LENGTH: usize = 64;
 
 const Q: [u64; 15] = [
     0x7311c2812425cfa0,
@@ -27,37 +31,116 @@ const T4: usize = 67;
 const RIGHT_SHIFTS: [usize; 16] = [10, 5, 13, 10, 11, 12, 2, 7, 14, 15, 7, 13, 11, 7, 6, 12];
 const LEFT_SHIFTS: [usize; 16] = [11, 24, 9, 16, 15, 9, 27, 15, 6, 2, 29, 8, 15, 5, 31, 9];
 
-const S_PRIM: u64 = 0x0123456789abcdef;
+const S_PRIM_0: u64 = 0x0123456789abcdef;
 const S_STAR: u64 = 0x7311c2812425cfa0;
 
-fn pad(input: &Vec<u8>) -> Vec<u32> {
-    let mut words: Vec<u32> = Vec::new();
-    words
-}
+#[derive(Debug)]
+struct MD6KeyError;
 
-fn compress(input: [u64; 74], r: usize) -> [u64; 16] {
-    // Q: 15 word u64 constant
-    // input contains:
-    // K: 8 word key
-    // U: 1 word unique ID
-    // V: 1 word control
-    // B: 64 word data block
-
-    // U can be seen as the level (1 byte) + position within level (7 bytes)
-    // V is [four 0 bits, 12 bits representing r, 8 bits representing L/mode, etc.] p.22
-
-    let c = 16;
-
-    // maybe can precompute all Si, if it is module
+impl fmt::Display for MD6KeyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Key length exceeds maximum allowable size of 64 bytes.")
+    }
 }
 
 pub struct MD6Key {
-    key: Vec<u8>,   // padded with 0 bytes until length 64 within md6
-    key_len: usize, // (key length in bytes, 0..=64)
+    key: Vec<u64>,
+    key_len: usize,
 }
 
 impl MD6Key {
-    pub fn new() -> Self {}
+    pub fn new(key: Option<Vec<u8>>) -> Result<Self, MD6KeyError> {
+        const MAX_LEN: usize = 64;
+        match key {
+            None => Ok(Self {
+                key: vec![0; 8],
+                key_len: 0,
+            }),
+            Some(key) => {
+                if key.len() > MAX_LEN {
+                    return Err(MD6KeyError);
+                } else {
+                    let mut key = key.clone();
+                    let key_len = key.len();
+                    key.resize(64, 0u8);
+                    let key: Vec<u64> = key
+                        .chunks_exact(8)
+                        .map(|chunk| u64::from_be_bytes(chunk.try_into().unwrap()))
+                        .collect();
+                    Ok(Self { key, key_len })
+                }
+            }
+        }
+    }
+}
+
+fn compress(input: &[u64; 74], r: usize) -> [u64; 16] {
+    let c: usize = 16;
+    let n: usize = 89;
+    let t: usize = r * c;
+
+    let mut a_vec: Vec<u64> = Vec::with_capacity(n);
+    a_vec.extend_from_slice(&Q);
+    a_vec.extend_from_slice(input);
+
+    let mut s_vec: Vec<u64> = vec![S_PRIM_0];
+    for i in 1..r {
+        s_vec.push(s_vec[i - 1].rotate_left(1) ^ (s_vec[i - 1] & S_STAR));
+    }
+
+    for i in n..(t + n) {
+        let mut x = s_vec[(i - n) / 16] ^ a_vec[i - n] ^ a_vec[i - T0];
+        x ^= (a_vec[i - T1] & a_vec[i - T2]) ^ (a_vec[i - T3] & a_vec[i - T4]);
+        x ^= x >> RIGHT_SHIFTS[(i - n) % 16];
+        a_vec.push(x ^ (x << LEFT_SHIFTS[(i - n) % 16]));
+    }
+
+    let mut chaining_value = [0u64; 16];
+    chaining_value.copy_from_slice(&a_vec[a_vec.len() - 16..]);
+    chaining_value
+}
+
+fn construct_v(r: usize, mode: usize, z: u64, p: usize, key_len: usize, d: usize) -> u64 {
+    // TODO
+    0
+}
+
+fn par(
+    prev_message: Vec<u64>,
+    d: usize,
+    key: MD6Key,
+    mode: usize,
+    r: usize,
+    level: u64,
+) -> Vec<u64> {
+    let prev_m = prev_message.len() * WORD_LENGTH;
+    let zero_words_to_add = 64 - (prev_message.len() % 64);
+    let mut prev_message = prev_message.clone();
+    prev_message.resize(prev_message.len() + zero_words_to_add, 0u64);
+
+    let new_message: Vec<u64> = Vec::new();
+
+    let b = 4096;
+    let j = (1).max(prev_m / (b * WORD_LENGTH));
+    for i in 0..j {
+        // STEP 1
+        let mut p = 0;
+        if i == j - 1 {
+            p = zero_words_to_add * WORD_LENGTH;
+        }
+
+        // STEP 2
+        let z: u64 = if j == 1 { 1 } else { 0 };
+
+        // STEP 3 - construct V function
+        let v: u64 = construct_v(r, mode, z, p, key.key_len, d);
+
+        // Step 4 - construct U
+        let u: u64 = level * 2u64.pow(56) + i as u64;
+
+        // Step 5 - combine values and call compress, appedn to message
+    }
+    new_message
 }
 
 pub struct MD6 {
@@ -72,7 +155,7 @@ impl MD6 {
     pub fn new(d: usize) -> Self {
         Self {
             d,
-            key: MD6Key::new(),
+            key: MD6Key::new(None).unwrap(),
             mode: 64,
             r: 40 + (d / 4),
             level: 0,
@@ -97,66 +180,32 @@ impl MD6 {
 
 impl MD6 {
     pub fn hash(&self, input: &Input) -> Output {
-        let input: Vec<u32> = pad(&input.bytes);
-        let mut state: Vec<u32> = vec![INIT_A, INIT_B, INIT_C, INIT_D];
-        for block in input.chunks(16) {
-            let state_copy: Vec<u32> = state.clone();
-            let mut idx: VecDeque<usize> = VecDeque::from([0, 1, 2, 3]);
-            for round in 0..64 {
-                let transform = match round {
-                    0..16 => f_transform,
-                    16..32 => g_transform,
-                    32..48 => h_transform,
-                    _ => i_transform,
-                };
-                state[idx[0]] = state[idx[1]].wrapping_add(
-                    state[idx[0]]
-                        .wrapping_add(transform(state[idx[1]], state[idx[2]], state[idx[3]]))
-                        .wrapping_add(block[K[round]])
-                        .wrapping_add(T[round])
-                        .rotate_left(S[round]),
-                );
-                idx.rotate_right(1);
-            }
-            for i in 0..4 {
-                state[i] = state[i].wrapping_add(state_copy[i]);
-            }
-        }
-        Output::from_u32_le(state)
+        let input: Vec<u32> = vec![];
+
+        Output::from_u32_le(vec![])
     }
 }
 
 // TODO: Implement general MD6 (byte level version)
 // TODO: Implement MD6_160, MD6_224, MD6_256, MD6_384, MD6_512
 // TODO: Implement test cases for each major version
-// TODO: Implement compression function
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn md6_160_works() {
-        
-    }
+    fn md6_160_works() {}
 
     #[test]
-    fn md6_224_works() {
-        
-    }
+    fn md6_224_works() {}
 
     #[test]
-    fn md6_256_works() {
-        
-    }
+    fn md6_256_works() {}
 
     #[test]
-    fn md6_384_works() {
-        
-    }
+    fn md6_384_works() {}
 
     #[test]
-    fn md6_512_works() {
-        
-    }
+    fn md6_512_works() {}
 }
