9aba4fb3738ff42f779303d8fc9120f89b358f7d
lvainio <lvainio@kth.se>
2024-11-11 20:08:11 +0100

    refactor: add reexports in crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-pub mod cipher;
-pub mod hash;
-pub mod util;
+mod cipher;
+mod hash;
+
+pub use hash::{Input, Output};
+
+pub use hash::md2::MD2;
+pub use hash::md4::MD4;
+pub use hash::md5::MD5;
+pub use hash::md6::{MD6Key, MD6, MD6_160, MD6_224, MD6_256, MD6_384, MD6_512};
+
+pub use hash::sha0::SHA0;
+pub use hash::sha1::SHA1;
+pub use hash::sha224::SHA224;
+pub use hash::sha256::SHA256;
+pub use hash::sha3::{SHA3_224, SHA3_256, SHA3_384, SHA3_512};
+pub use hash::sha384::SHA384;
+pub use hash::sha512::SHA512;
+pub use hash::sha512_224::SHA512_224;
+pub use hash::sha512_256::SHA512_256;
